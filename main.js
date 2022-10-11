f587b208bc1ba56e3c3247cdf68001699d0312c1
abderrahim-ouertani <abderahim.ouertani@gmail.com>
2022-10-11 11:32:46 +0100

    this is how far i went with this project if i had more time i would make it better

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 $(".image").hide()
-var upper = 100;
+var upper = 10;
 var rand = randInt(upper);
 var counter = 0;
 var record = 5;
 console.log(rand)
 
+
 $("input").change(guessMyNumber)
 function guessMyNumber(){
+
+    // here i will assign the value of the input to a variable
   var myInput = $("input").val()
+  // then i will multiply it by 1 to convert it from string to a number so that i can work with it
 myInput=myInput*1
 
-
-  if (counter < 5) {
+// here is the first condition if the user gives a number higher than the upperbound an alert will pop out to him to tell him that he needs to give a number less than 10
+  if (counter < 2) {
+    // we will start counting from here we won't put counter in the end because we need the value and we don't want it
     counter++;
     var newCount = counter;
     if (myInput > upper) {
@@ -19,18 +24,32 @@ myInput=myInput*1
     } else if (myInput === rand) {
       upper++;
       rand = randInt(upper);
+      console.log(rand)
       if (counter < record) {
         record = counter;
         counter = 0;
-        $(".image").fadeIn(5000)
-        $(".image").fadeOut(5000)
+       $("audio")[1].play() 
+        $(".image").fadeIn(2000)
+        $(".image").fadeOut(2000)
+        $("#photo").hide()
+
+        $("#celebration").fadeIn(2000)
+        $("#celebration").fadeOut(2000)
+        
+
         window.alert( 
           "You guessed my number! in " +  newCount +
           " times  NEW RECORD SET " +  record
         );
-      
 
+     console.log(counter);
       }
+      setTimeout(function(){
+        $("#photo").fadeIn(2000)
+      }, 4000)
+      // i put counter =0 here to let the counter return to 0 even it's not a new record
+      counter = 0;
+
       window.alert( "You guessed my number! in " + newCount + " times");
     } else if (myInput!=rand){
 
@@ -38,9 +57,10 @@ myInput=myInput*1
   } }else {
     upper--;
     rand = randInt(upper);
-    return "game over";
-  }
-}
+     window.alert("game over");
+
+}}
+
 
 function randInt(n) {
   return Math.floor(Math.random() * (n + 1));
